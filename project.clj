3c15086ad4bf96ba182394b04939b2d8183d6f01
ingesolvoll <inge@semion.ca>
2020-10-14 15:17:05 +0200

    Bumped some deps

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/ingesolvoll/re-chain"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597"]
-                 [re-frame "0.10.6"]
+                 [re-frame "1.0.0"]
                  [expound "0.8.2"]
                  [day8.re-frame/test "0.1.5"]]
 
-  :plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url           "https://clojars.org/repo"
                                     :username      :env/clojars_username
