2af406242f379270c6d043f4d12efd62c3cacf53
ingesolvoll <inge.solvoll@gmail.com>
2021-03-07 15:22:13 +0100

    Release 1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-chain "1.3-SNAPSHOT"
+(defproject re-chain "1.3"
   :description "Easy chaining of re-frame events"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
